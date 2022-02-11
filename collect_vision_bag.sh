740183bdcce41e4bff433434c87745c8cab357db
Jason X <jasonxie@live.unc.edu>
2022-02-11 14:12:29 -0800

    refine scripts

--- collect_vision_bag.sh
+++ collect_vision_bag.sh
@@ -16,10 +16,24 @@ CONTAINER_ID=$(docker ps --filter ancestor=dustynv/ros:foxy-slam-l4t-r32.5.0 -q)
 docker exec $CONTAINER_ID bash -c "source ros_entrypoint.sh;\
                                 cd home/packbionics/dev_ws/bag_files;\
                                 ros2 bag record -o ${bag_name} -a" &
+BAG_PID=$!
+sleep 3
 # Launch zed_wrapper
 docker exec $CONTAINER_ID bash -c "source ros_entrypoint.sh;\
                                 source home/packbionics/dev_ws/install/setup.bash;\
                                 ros2 launch jetleg_vision jetleg_zed2i.launch.py" &
+WRAPPER_PID=$!
+
+abort_script() {
+    echo "Aborting bag record..."
+    kill -9 $BAG_PID
+    kill -9 $WRAPPER_PID
+    docker stop $CONTAINER_ID
+    sudo rm -r ~/dev_ws/bag_files/${bag_name}
+    exit
+}
+
+trap "abort_script" SIGINT SIGTERM
 
 # Wait for recording to finish
 sleep $(( $duration + 5 ))
@@ -27,5 +41,9 @@ docker stop $CONTAINER_ID
 
 # Copy bag file to storage machine
 scp -r ~/dev_ws/bag_files/$bag_name $local_username@$local_ip:~
-
-echo "Bag file saved to ~/$bag_name on $local_ip"
+if [ "$?" -eq "0" ];
+then
+    echo "Bag file saved to ~/$bag_name on $local_ip"
+else
+    echo "Bag file was NOT saved"
+fi
